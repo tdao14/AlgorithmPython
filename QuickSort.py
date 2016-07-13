7a0f7ba3c1cf7c548da99800f46f359a9be3d04c
tdao14 <tridao1412@gmail.com>
2016-07-13 13:13:08 -0700

    Add files via upload

---
+++ QuickSort.py
@@ -0,0 +1,39 @@
+def partition(a, first, last):
+    left = first + 1
+        right = last
+        pivotValue = a[first]
+        
+        done = False
+        
+        while not done:
+            
+            while left <= right and a[left] <= pivotValue :
+                left += 1
+                
+                while left <= right and a[right] >= pivotValue:
+                    right -= 1
+                
+                if left <= right:
+                    temp = a[left]
+                        a[left] = a[right]
+                        a[right] = temp
+                else:
+                    done = True
+
+
+temp = a[first]
+    a[first] = a[right]
+        a[right] = temp
+        
+        return right
+
+def quickSort(a):
+    quickSortHelper(a, 0, len(a)-1)
+
+
+def quickSortHelper(a, first, last):
+    if first < last:
+        splitPoint = partition(a, first , last)
+            
+            quickSortHelper(a, first, splitPoint-1)
+                quickSortHelper(a, splitPoint+1, last)
