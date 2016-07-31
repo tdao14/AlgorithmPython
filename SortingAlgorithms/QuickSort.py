4175b98209defeeea3e664a3cd3d51dfcc03b895
tdao14 <tridao1412@gmail.com>
2016-07-31 16:29:05 -0700

    Update QuickSort.py

--- SortingAlgorithms/QuickSort.py
+++ SortingAlgorithms/QuickSort.py
@@ -1,31 +1,31 @@
 def partition(a, first, last):
     left = first + 1
-        right = last
-        pivotValue = a[first]
+    right = last
+    pivotValue = a[first]
         
-        done = False
+    done = False
         
-        while not done:
+    while not done:
             
-            while left <= right and a[left] <= pivotValue :
-                left += 1
+        while left <= right and a[left] <= pivotValue :
+            left += 1
                 
-                while left <= right and a[right] >= pivotValue:
-                    right -= 1
+            while left <= right and a[right] >= pivotValue:
+                right -= 1
                 
-                if left <= right:
-                    temp = a[left]
-                        a[left] = a[right]
-                        a[right] = temp
-                else:
-                    done = True
+            if left <= right:
+                temp = a[left]
+                a[left] = a[right]
+                a[right] = temp
+            else:
+                done = True
 
 
-temp = a[first]
+    temp = a[first]
     a[first] = a[right]
-        a[right] = temp
+    a[right] = temp
         
-        return right
+    return right
 
 def quickSort(a):
     quickSortHelper(a, 0, len(a)-1)
