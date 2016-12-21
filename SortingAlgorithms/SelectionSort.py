dac53d2c4e911bc4b870c2d1e7b98e72e8c089c9
tdao14 <tridao1412@gmail.com>
2016-12-21 01:34:53 -0800

    Update SelectionSort.py

--- SortingAlgorithms/SelectionSort.py
+++ SortingAlgorithms/SelectionSort.py
@@ -1,15 +1,15 @@
 def selectionSort(a):
     i = 0
     n = len(a)
-        while i < n-1:
-            s = i
-            for j in range(i+1, n):
-                 if a[s] > a[j]:
-                    s = j
+    while i < n-1:
+        s = i
+        for j in range(i+1, n):
+            if a[s] > a[j]:
+               s = j
                 
-            if s != i:
-               temp = a[s]
-               a[s] = a[i]
-               a[i] = temp
+         if s != i:
+            temp = a[s]
+            a[s] = a[i]
+            a[i] = temp
                 
-            i+=1
+         i+=1
